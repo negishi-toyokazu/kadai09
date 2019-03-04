0e7f10760e9867c211977c9565214236c1c61337
negitoyo <negishitoyokazu@toyo-pc.local>
2019-03-04 10:53:49 +0900

    kadai10

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    //Q4
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\Profilecontroller@edit');
+});
+
+
+//Q3
+Route::get('XXX', 'AAAController@bbb');
