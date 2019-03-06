f5ee7371c1607b81c6d21d6d3a7631c32abc3ce2
negitoyo <negishitoyokazu@toyo-pc.local>
2019-03-06 16:34:25 +0900

    php17

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,19 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'middleware' =>  'auth'], function () {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
-
     Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+
 
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
-
     Route::get('profile/edit', 'Admin\Profilecontroller@edit');
     Route::post('profile/edit', 'Admin\Profilecontroller@update');
 });
 
 
-//Q3
-Route::get('XXX', 'AAAController@bbb');
+
 
 Auth::routes();
 
