ce020cf946368681ea72cdafc6581e0440fbb4dd
negitoyo <negishitoyokazu@toyo-pc.local>
2019-03-08 18:33:43 +0900

    kadai19-2

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'NewsController@index');
-Route::get('/', 'NewsController@profile');
+Route::get('/profile', 'NewsController@profile');
